ba257cc481e33689c222c91bbf9b7db24603ae3f
saurabh chuadhary <supersaurabh017@gmail.com>
2023-02-17 23:37:35 +0530

    initial commit

--- src/project.js
+++ src/project.js
@@ -22,7 +22,8 @@ function Project() {
                             <h3 class="main_content-project-content-heading">Memes Generator</h3>
                             <p class="main_content-project-content-paragraph">Memes Generator is webpage where you can get memes template and can make your own memes by editing it on webpage <b>,</b> After that you can download it and share it with your friends <b>.</b></p>
                             {/* <a class="main_content-project-content-button-code" href='https://github.com/saurabhcr007/memes-generator'>CODE</a> */}
-                            <a class="main_content-project-content-button-site" href='https://memegenerator007.netlify.app/'>Live Link</a>
+                            <a class="main_content-project-content-button-site" href='https://memegenerator007.netlify.app/' target="_blank">Live Link</a>
+                            {/* <button class="main_content-project-content-button-site" onclick="window.open('https://google.com', '_blank')">Live Link</button> */}
                         </div>
                     </div>
                     <div class="main_content-project-row">
@@ -33,7 +34,7 @@ function Project() {
                             <h3 class="main_content-project-content-heading">Password Generator</h3>
                             <p class="main_content-project-content-paragraph">Password Generator is a Webpage where you can crate a unique password <b>,</b> which can you use it by copying it <b>,</b> you can set the length of the password <b>.</b> </p>
                             {/* <a class="main_content-project-content-button-code" href='https://github.com/saurabhcr007/Password-Generator-by-html-css-js-'>CODE</a> */}
-                            <a class="main_content-project-content-button-site" href='https://saurabhcr007.github.io/Password-Generator-by-html-css-js-/'>Live Link</a>
+                            <a class="main_content-project-content-button-site" href='https://saurabhcr007.github.io/Password-Generator-by-html-css-js-/' target="_blank">Live Link</a>
                         </div>
                     </div>
                     <div class="main_content-project-row">
@@ -42,9 +43,9 @@ function Project() {
                         </div>
                         <div class="main_content-project-content">
                             <h3 class="main_content-project-content-heading">EduLABS (User Interactive Lab Simulator)</h3>
-                            <p class="main_content-project-content-paragraph">EduLABS is a android application <b>,</b> </p>
+                            <p class="main_content-project-content-paragraph">EduLABS is a android application <b>,</b>It is a virtual lab simulation application for interactive experiments and research with a user-friendly interface and a variety of laboratory equipment<b>.</b> </p>
                             {/* <a class="main_content-project-content-button-code" href='https://github.com/saurabhcr007/EduLABs'>CODE</a> */}
-                            <a class="main_content-project-content-button-site" href='https://drive.google.com/drive/folders/1ZGQsg3W5EHN6oI0OlXag-pqLAEpV5I99?usp=share_link'>Download Link</a>
+                            <a class="main_content-project-content-button-site" href='https://drive.google.com/drive/folders/1VeFXubCw4QUZ9QEamGPOndRyvNLXKw3f?usp=sharing' target="_blank">Download Link</a>
                         </div>
                     </div>
                 </div>
